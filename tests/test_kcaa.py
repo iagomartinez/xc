acf6b87e2d2d9a7c9afe18353f88059fd81c7ee4
Iago Martínez <iago@fundapps.co>
2022-01-28 08:36:14 +0000

    Extract all entries from file

--- tests/test_kcaa.py
+++ tests/test_kcaa.py
@@ -3,15 +3,20 @@ import unittest
 from pathlib import Path
 import re
 import pandas as pd
-THIS_DIR = Path(__file__).parent
+ROOT = Path(__file__).parent.parent
 
 sys.path.insert(1, '..')
 from xc import kcaa
 
 class TestKentXc(unittest.TestCase):
-    def test_parse_entries(self):
+    def test_extract_row(self):
         testdata = [('1 887 Jamie Goodge TON 41:41 61 741 Huntley Roberts (M35) DART 49:58', 2), ('144 645 Steve Hodges (M55) A&D 57:45',1)]
         for line, expectedcount in testdata:
             with self.subTest(f'testing {line}->{expectedcount}'):
-                matches = kcaa.parse_entries(line)
-                self.assertEquals(expectedcount, len(matches))
+                matches = kcaa.extract_row(line)
+                self.assertEqual(expectedcount, len(matches))
+    
+    def test_extract_file(self):
+        file = ROOT / 'data/kent_champs_men.txt'
+        results = kcaa.extract_file(file)
+        self.assertEqual(203, len(results))
--- xc/kcaa.py
+++ xc/kcaa.py
@@ -1,6 +1,13 @@
 import re
 import pandas as pd
 
-def parse_entries(line):
+def extract_row(line):
     resultex = r'\d{1,3}\s\d{1,3}\s.*?\d{1,2}\:\d{1,2}'
     return re.findall(resultex,line)
+
+def extract_file(file):
+    results = []
+    with open(file, 'r', newline='', encoding='utf-8') as f:
+        for line in f:
+            results.extend(extract_row(line))
+    return results
