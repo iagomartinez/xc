033a7f27055c4e724efd67fea93478da57b20b26
Iago Martínez <iago@fundapps.co>
2022-01-28 14:39:16 +0000

    First pass at parsing fields from input row - no age category

--- tests/test_kcaa.py
+++ tests/test_kcaa.py
@@ -9,14 +9,29 @@ sys.path.insert(1, '..')
 from xc import kcaa
 
 class TestKentXc(unittest.TestCase):
-    def test_extract_row(self):
+    def test_extract_result(self):
         testdata = [('1 887 Jamie Goodge TON 41:41 61 741 Huntley Roberts (M35) DART 49:58', 2), ('144 645 Steve Hodges (M55) A&D 57:45',1)]
         for line, expectedcount in testdata:
             with self.subTest(f'testing {line}->{expectedcount}'):
-                matches = kcaa.extract_row(line)
+                matches = kcaa.extract_result(line)
                 self.assertEqual(expectedcount, len(matches))
     
     def test_extract_file(self):
         file = ROOT / 'data/kent_champs_men.txt'
         results = kcaa.extract_file(file)
         self.assertEqual(203, len(results))
+
+    def test_parse_athletes(self):
+        file = ROOT / 'data/kent_champs_men.txt'
+        results = kcaa.extract_file(file)
+        parsed = kcaa.parse(results)
+        self.assertEqual(203, len(parsed))
+
+    def test_ResultParser(self):
+        rp = kcaa.ResultParser()
+        testdata = [('16 812 Christopher McGurk KENT 45:10', {'position':'16', 'name': 'Christopher McGurk', 'club': 'KENT', 'time': '45:10'})]
+        for input, expected in testdata:
+            with self.subTest(f'testing {input}->{expected}'):
+                parsed = rp.parse(input)
+                self.assertDictEqual(expected, parsed)
+        
--- xc/kcaa.py
+++ xc/kcaa.py
@@ -1,7 +1,9 @@
+from mimetypes import init
 import re
+from types import new_class
 import pandas as pd
 
-def extract_row(line):
+def extract_result(line):
     resultex = r'\d{1,3}\s\d{1,3}\s.*?\d{1,2}\:\d{1,2}'
     return re.findall(resultex,line)
 
@@ -9,5 +11,21 @@ def extract_file(file):
     results = []
     with open(file, 'r', newline='', encoding='utf-8') as f:
         for line in f:
-            results.extend(extract_row(line))
+            results.extend(extract_result(line))
     return results
+
+class ResultParser():
+    RESULT_REGEX = r'(?P<position>\d{1,3})\s(?:\d{1,3})\s(?P<name>.*?)\s(?P<club>[\w&]*)\s(?P<time>\d{1,2}\:\d{1,2})'
+
+    def __init__(self):
+        self.resultx = re.compile(self.RESULT_REGEX)
+
+    def parse(self, result):
+        m = self.resultx.match(result)
+        if not m:
+            raise(f'failed to parse row {result}')
+        return m.groupdict()
+
+def parse(results):
+    rp = ResultParser()
+    return [rp.parse(result) for result in results]
