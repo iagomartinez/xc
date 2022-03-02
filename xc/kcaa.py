eb5f9557170842ac37f54e2e880a82841a29b86f
Iago Martínez <iago@fundapps.co>
2022-03-02 22:35:52 +0000

    Finish kcaa main function

--- xc/kcaa.py
+++ xc/kcaa.py
@@ -1,5 +1,10 @@
 import re
 import pandas as pd
+import sys
+from pathlib import Path
+from IPython.display import display
+
+ROOT = Path(__file__).parent.parent
 
 def extract_result(line):
     resultex = r'\d{1,3}\s\d{1,3}\s.*?\d{1,2}\:\d{1,2}'
@@ -27,3 +32,30 @@ class ResultParser():
 def parse(results):
     rp = ResultParser()
     return [rp.parse(result) for result in results]
+
+def as_dataframe(parsed):
+    df = pd.DataFrame.from_records(parsed)
+    df = df.loc[df.club == 'KENT']
+    df = df.apply(pd.to_numeric, errors='ignore')
+    df.sort_values(by='position', inplace=True)
+    return df
+
+def main():
+    rp = ResultParser()
+
+    data = ROOT / 'data'
+
+    results = extract_file(data / 'kent_champs_men.txt')
+    df_men = as_dataframe(parse(results))
+    assert len(df_men.index) == 21
+    df_men.to_csv(data / 'parsed_kcaa_men.csv', index=False)
+
+    results = extract_file(data / 'kent_champs_women.txt')
+    df_men = as_dataframe(parse(results))
+    assert len(df_men.index) == 13
+    df_men.to_csv(data / 'parsed_kcaa_women.csv', index=False)
+
+    print('Export completed!')
+
+if __name__ == '__main__':
+    sys.exit(main())    
